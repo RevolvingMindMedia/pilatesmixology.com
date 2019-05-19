c8023db56c64999f0db017e333ca684f80855eb6
Rahul Dhir <rahul.dhir96@gmail.com>
2019-05-19 15:24:42 -0500

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+            <span className="icon fa-diamond"></span>
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1>Pilates Mixology</h1>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="https://clients.mindbodyonline.com/LoginLaunch?studioid=256008" target="_blank">Book Now</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: PropTypes.func,
+    timeout: PropTypes.bool
+}
+
+export default Header
